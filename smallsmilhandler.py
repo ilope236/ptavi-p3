67760df4d72e9117e6c561ca4ee708e4a7d2a862
irene lope rodriguez <ilope@iota21.aulas.gsyc.es>
2014-10-06 10:54:54 +0200

    Primera sincronizacion

---
+++ smallsmilhandler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler)
+
+    def __init__(self):
+        self.root-layout
+        self.region
