f9e11ae7a8d7aeb34dedfbe253a8f04dbf3922de
Irene <irene@ubuntu.(none)>
2014-10-12 13:16:52 +0200

    Ejercicio 5 + pep8

--- karaoke.py
+++ karaoke.py
@@ -4,7 +4,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import sys
-
+import os
 
 try:
     fichero = sys.argv[1]
@@ -22,6 +22,10 @@ lista_dic = sHandler.get_tags()
 for diccionario in lista_dic:
     print diccionario['name'] + "\t",
     for key in diccionario:
-        if key != 'name' and diccionario[key] != "":
+        if key != 'name' and diccionario[key]:
             print key + "=" + diccionario[key] + "\t",
+            if key == 'src' and diccionario[key][:7] == 'http://':
+                os.system("wget -q " + diccionario[key])
+                lista_barras = diccionario[key].split('/')
+                diccionario[key] = lista_barras[-1]
     print
