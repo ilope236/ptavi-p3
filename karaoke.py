ae3d5a288a7465ce5be91bfbf177bb12b6ad9843
Irene <irene@ubuntu.(none)>
2014-10-12 14:23:32 +0200

    Ejercicio 6 + pep8

--- karaoke.py
+++ karaoke.py
@@ -6,26 +6,42 @@ import smallsmilhandler
 import sys
 import os
 
-try:
-    fichero = sys.argv[1]
-except IndexError:
-    print "Usage: python karaoke.py file.smil."
-    raise SystemExit
 
-parser = make_parser()
-sHandler = smallsmilhandler.SmallSMILHandler()
-parser.setContentHandler(sHandler)
-parser.parse(open(fichero))
+class KaraokeLocal():
 
-lista_dic = sHandler.get_tags()
+    def __init__(self, fichero):
+        parser = make_parser()
+        sHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(sHandler)
+        parser.parse(open(fichero))
+        self.lista_dic = sHandler.get_tags()
 
-for diccionario in lista_dic:
-    print diccionario['name'] + "\t",
-    for key in diccionario:
-        if key != 'name' and diccionario[key]:
-            print key + "=" + diccionario[key] + "\t",
-            if key == 'src' and diccionario[key][:7] == 'http://':
-                os.system("wget -q " + diccionario[key])
-                lista_barras = diccionario[key].split('/')
-                diccionario[key] = lista_barras[-1]
+    def __str__(self):
+        for diccionario in self.lista_dic:
+            print diccionario['name'] + "\t",
+            for key in diccionario:
+                if key != 'name' and diccionario[key]:
+                    print key + "=" + diccionario[key] + "\t",
+            print
+
+    def do_local(self):
+        for diccionario in self.lista_dic:
+            for key in diccionario:
+                if key == 'src' and diccionario[key][:7] == 'http://':
+                    os.system("wget -q " + diccionario[key])
+                    lista_barras = diccionario[key].split('/')
+                    diccionario[key] = lista_barras[-1]
+
+if __name__ == "__main__":
+
+    try:
+        fichero = sys.argv[1]
+    except IndexError:
+        print "Usage: python karaoke.py file.smil."
+        raise SystemExit
+
+    karaoke = KaraokeLocal(fichero)
+    karaoke.__str__()
     print
+    karaoke.do_local()
+    karaoke.__str__()
